f83cb4132f5f6202c5877982bddf43d36c578966
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-24 21:21:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function printBadges(array){
 function tailsNeverFails() {
     let tails = 0; 
     while (Math.random () >= 0.5) {
-      tails++; }
+      tails++; 
+      
+    }
     
     return ("You got "+ tails + " tails in a row!")
 }
