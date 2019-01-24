1049b2074a07b13da79a7a38390f20a544fbdc82
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-24 21:08:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function printBadges(array){ 
+  for (let i = 0; i < array.length; i+=1) { 
+    console.log('Welcome ${array[i]}! You are employee #${i + 1}');
+  }
+  return array; 
+}
+
+function maybeTrue() {
+    return Math.random() >= 0.5;
+}
+ 
+while (maybeTrue()) {
+    console.log('And I ran... I ran so far away!');
+}
